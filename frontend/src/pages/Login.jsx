65c5908bf55161372563088e4ffe9c08012f479b
Suvin S <your-email@example.com>
2026-06-28 14:49:03 +0530

    fixed img issue

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import bgImage from "../assets/bg-Img.jpg"; 
+import bgImage from "../assets/bg-img.jpg"; 
 
 function Login() {
   const [email, setEmail] = useState("");
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import bgImage from "../assets/bg-Img.jpg"; 
+import bgImage from "../assets/bg-img.jpg"; 
 
 function Register() {
   const [name, setName] = useState("");
